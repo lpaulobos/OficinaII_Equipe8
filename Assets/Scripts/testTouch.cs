83688d4945104c868de9058569bbc37fc985a8f6
Luís Paulo Braga <lpaulobos2010@gmail.com>
2015-08-31 15:37:51 -0300

    Version 1.0.3
    Project With Levels 1,2 and 3.  Place holders

---
+++ Assets/Scripts/testTouch.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections;
+
+public class testTouch : MonoBehaviour {
+	
+	public Vector3 fingerPos;
+	public Vector3 realWorldPos;
+	void Update()
+	{
+		if(Input.touchSupported)
+		{
+			fingerPos = Input.GetTouch(0).position;
+			Vector3 pos = fingerPos;
+			realWorldPos = Camera.main.ScreenToWorldPoint(pos);
+			realWorldPos.z = 0;
+			transform.localPosition = realWorldPos; 
+		}else
+		{
+			if(Input.GetKey(KeyCode.A))
+			{transform.position = new Vector3 (this.transform.position.x - 0.05f,
+											   this.transform.position.y,
+				                               this.transform.position.z);
+         	}
+			if((Input.GetKey(KeyCode.D))){
+				transform.position = new Vector3 (this.transform.position.x + 0.05f,
+				                                  this.transform.position.y,
+				                                  this.transform.position.z);
+			}
+			if((Input.GetKey(KeyCode.W))){
+				transform.position = new Vector3 (this.transform.position.x,
+				                                  this.transform.position.y + 0.05f,
+				                                  this.transform.position.z);
+			}
+			
+			if((Input.GetKey(KeyCode.S))){
+				transform.position = new Vector3 (this.transform.position.x,
+				                                  this.transform.position.y - 0.05f,
+				                                  this.transform.position.z);
+        	}
+		}
+		
+	}
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "Dirt")
+		{
+			if(GameObject.Find (col.gameObject.name).GetComponent<dirtBehaviour>().numBrushesOn < 
+			   GameObject.Find (col.gameObject.name).GetComponent<dirtBehaviour>().numBrushes)
+				GameObject.Find (col.gameObject.name).GetComponent<dirtBehaviour>().numBrushesOn +=1;
+			else Destroy(col.gameObject);
+		}
+	}
+	
+}
+
+
